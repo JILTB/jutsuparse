f2dad0e455ee731a1b1eee6261141accc1d97579
JILTB <71900962+JILTB@users.noreply.github.com>
2021-09-15 20:35:10 +0300

    First Version

---
+++ all.py
@@ -0,0 +1,7 @@
+import narutoshippuden1_53
+import narutoshippuden54_89
+import narutoshippuden90_500
+
+narutoshippuden1_53.download()
+narutoshippuden54_89.download()
+narutoshippuden90_500.download()
